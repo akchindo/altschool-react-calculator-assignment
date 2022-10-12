951819d9bb9e6419aea457d54830d730b8c2b351
akchindo <abdullahichindo16@gmail.com>
2022-10-12 17:04:19 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,72 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [digit, setDigit] = useState([]);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <section id='calculator'>
+        <div className='cal-container'>
+          <h1 className='contact-head'>Calculator</h1>
+          <div className='cal-value'>
+            <input type='text' className='caltext' placeholder={digit} />
+          </div>
+          <div className='grid-values'>
+            <div className='val' onClick={()=>setDigit(digit + "*")} >
+              <p value='*' >*</p>
+            </div>
+            <div className='val' onClick={()=>setDigit(digit + "/")} >
+              <p value='/' >/</p>
+            </div>
+            <div className='val' onClick={()=>setDigit(digit + "-")} >
+              <p value='-'  >-</p>
+            </div>
+            <div className="val" onClick={()=>setDigit(digit + "+")} >
+              <p value='+'   >+</p>
+            </div>
+            <div className='val' onClick={()=>setDigit(digit + "%")} >
+              <p value='%'>%</p>
+            </div>
+            <div className='val' onClick={()=>setDigit(digit + "9")} >
+              <p value='9'>9</p>
+            </div>
+            <div className='val' onClick={()=>setDigit(digit + "6")} >
+              <p value='6'>6</p>
+            </div>
+            <div className='val' onClick={()=>setDigit(digit + "7")} >
+              <p value='7'>7</p>
+            </div>
+            <div className='val' onClick={()=>setDigit(digit + "8")} >
+              <p value='8'>8</p>
+            </div>
+            <div className='val' onClick={()=>setDigit(digit + "3")} >
+              <p value='3'>3</p>
+            </div>
+            <div className='val' onClick={()=>setDigit(digit + "4")} >
+              <p value='4'>4</p>
+            </div>
+            <div className='val' onClick={()=>setDigit(digit + "5")} >
+              <p value='5'>5</p>
+            </div>
+            <div className='val'  onClick={()=>setDigit(digit + "0")} >
+              <p value='0'>0</p>
+            </div>
+            <div className='val' onClick={()=>setDigit(digit + "1")} >
+              <p value='1'>1</p>
+            </div>
+            <div className='val' onClick={()=>setDigit(digit + "2")} >
+              <p value='2'>2</p>
+            </div>
+            <div className='val' onClick={(prev)=>setDigit(digit + prev)}>
+              <p value='='>=</p>
+            </div>
+            <div className='val' onClick={()=>setDigit("")} >
+              <p value='C'>C</p>
+            </div>
+          </div>
+        </div>
+      </section>
     </div>
   );
 }
